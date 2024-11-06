233850d0a7227f4cd78ec0924d55cacbb043c69d
TheLouitosIA <louisv2303@gmail.com>
2024-11-06 18:14:37 +0100

    Update start_app.sh

--- start_app.sh
+++ start_app.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-# Activer l'environnement virtuel
-source venv/bin/activate
+# Activation de l'environnement virtuel
+if [[ "$OSTYPE" == "msys" ]]; then
+    # Windows
+    source venv/Scripts/activate
+else
+    # Unix-based
+    source venv/bin/activate
+fi
 
-# Démarrer l'application FastAPI
-uvicorn app:app --host 0.0.0.0 --port 80 &
+# Lancer l'application FastAPI
+uvicorn app:app --host 0.0.0.0 --port 8000 &
 
 # Lancer ngrok avec le sous-domaine statique configuré dans ngrok.yml
 ngrok start default
